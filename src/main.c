a897dd62ddeb1a7875fe943286da2825f2eac5f9
trodion <rodion.toporoff@yandex.ru>
2023-10-13 17:40:00 +0300

    Initial commit

--- src/main.c
+++ src/main.c
@@ -1,101 +1,71 @@
-#include "stm32f100xb.h"
+#include "stm32f1xx.h"
 
-void init_pin();
-void init_NVIC();
-void msDelay(int ms);
-void init_TIM3();
-void init_TIM4();
-void init_UART();
-void send_byte(uint8_t byte);
+#define START (TIM3->CR1 |= TIM_CR1_CEN)
 
-void EXTI9_5_IRQHandler(void) {
-    if (EXTI->PR & EXTI_PR_PR5){
-        /* Прерывание от PC5 - SW */
-        GPIOC->ODR ^= GPIO_ODR_ODR9;
-        msDelay(500);
-        EXTI->PR |= EXTI_PR_PR5;
-    }
+void init_pin(); /* Настройка пинов */
+void init_NVIC(); /* Система прерываний */
+void init_TIM3(); /* Формирование PWM для управления двигателем */
+void init_TIM4(); /* Счет испульсов от энкодера */
+void msDelay(uint32_t ms); /* Функция задержки */
+
+
+void TIM4_IRQHandler() {
+    TIM3->CCR3 = TIM4->CNT;
+    GPIOC->ODR ^= GPIO_ODR_ODR9;
+    msDelay(150);
+    // Сбросить флаг прерывания
+    TIM4->SR &= ~TIM_SR_TIF;
 }
 
-void TIM4_IRQHandler(void) {
-    uint8_t cur_cnt = TIM4->CNT;
+int main(void) {
+    // Включение тактирования портов B, C, AFIOEN
+    RCC->APB2ENR |= RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN | RCC_APB2ENR_AFIOEN;
+    // Включение тактирования TIM3, TIM4 
+    RCC->APB1ENR |= RCC_APB1ENR_TIM3EN | RCC_APB1ENR_TIM4EN;
     
-    if (TIM4->SR & TIM_SR_TIF) {
-        send_byte(cur_cnt);
-        if (cur_cnt >= 0 && cur_cnt <= 4) cur_cnt = 0;
-        if (cur_cnt >= 13 || cur_cnt <= 26) cur_cnt <<= 1;
-        TIM3->CCR3 = (cur_cnt == 1 ? 0 : cur_cnt);
-        msDelay(150);
-        // Сбросить флаг прерывания
-        TIM4->SR &= ~TIM_SR_TIF;
-    }
-}
-
-int main() {
-    RCC->APB2ENR |= RCC_APB2ENR_IOPAEN | RCC_APB2ENR_IOPBEN | RCC_APB2ENR_IOPCEN | RCC_APB2ENR_AFIOEN;
-    RCC->APB1ENR |= RCC_APB1ENR_TIM3EN | RCC_APB1ENR_TIM4EN | RCC_APB1ENR_USART3EN;
-
     init_pin();
     init_NVIC();
-    init_TIM3(); init_TIM4();
-    init_UART();
-    
-    TIM3->CR1 |= TIM_CR1_CEN;
-    TIM4->CR1 |= TIM_CR1_CEN;
-    
+    init_TIM3();
+    init_TIM4();
+
+    START;
+
     while(1) {
+
     }
+    return 0;
 }
 
 void init_pin() {
-    /* PC8 - BLUE LED, TIM3_Ch3 */
+    /* PC8 - Alternative Function Push-Pull,  TIM3_Ch3 */
     GPIOC->CRH |= GPIO_CRH_CNF8_1 | GPIO_CRH_MODE8_1 | GPIO_CRH_MODE8_0;
     GPIOC->CRH &= ~GPIO_CRH_CNF8_0;
+
     AFIO->MAPR |= AFIO_MAPR_TIM3_REMAP;
 
-    /* PC9 - GREEN LED */
+
     GPIOC->CRH |= GPIO_CRH_MODE9_1 | GPIO_CRH_MODE9_0;
     GPIOC->CRH &= ~GPIO_CRH_CNF9_1 & ~GPIO_CRH_CNF9_0;
-    
-    /* PB6 - A, Input Pull-up */
-    GPIOB->CRL |= GPIO_CRL_CNF6_1;
-    GPIOB->CRL &= ~GPIO_CRL_CNF6_0 & ~GPIO_CRL_MODE6_1 & ~GPIO_CRL_MODE6_0;
-    GPIOB->ODR |= GPIO_ODR_ODR6;
 
-    /* PB7 - B, Input Pull-up */
+    /* PB6(A) - TIM4_Ch1, Input PU */
+    GPIOB->CRL |= GPIO_CRL_CNF6_1;
+    GPIOB->CRL &= ~GPIO_CRL_CNF6_0 & ~GPIO_CRL_MODE6_1  & ~GPIO_CRL_MODE6_0;
+    GPIOB->ODR |= GPIO_ODR_ODR6;
+    /* PB7(B) - TIM4_Ch2, Input PU */
     GPIOB->CRL |= GPIO_CRL_CNF7_1;
     GPIOB->CRL &= ~GPIO_CRL_CNF7_0 & ~GPIO_CRL_MODE7_1 & ~GPIO_CRL_MODE7_0;
     GPIOB->ODR |= GPIO_ODR_ODR7;
-    
-    /* PC5 - SW, Input Pull-up */
-    GPIOC->CRL |= GPIO_CRL_CNF5_1;
-    GPIOC->CRL &= ~GPIO_CRL_CNF5_0 & ~GPIO_CRL_MODE5_1 & ~GPIO_CRL_MODE5_0;
-    GPIOC->ODR |= GPIO_ODR_ODR5;
-
-    /* PB10 - USART3_TX, передающий канал, AF push-pull */
-    GPIOB->CRH |= GPIO_CRH_CNF10_1 | GPIO_CRH_MODE10_1 | GPIO_CRH_MODE10_0;
-    GPIOB->CRH &= ~GPIO_CRH_CNF10_0;
 }
 
 void init_NVIC() {
-    /* Настойка SW, кнопки */
-    /* Разешить прерывания от 5 линии */
-    EXTI->IMR |= EXTI_IMR_IM5;
-    /* По нарастающему фронту сигнала */
-    EXTI->RTSR |= EXTI_RTSR_RT5;
-    /* Соединить 5 линию с портом C (т.е. PC5) */
-    AFIO->EXTICR[1] |= AFIO_EXTICR2_EXTI5_PC;
-    
-    NVIC_SetPriority(EXTI9_5_IRQn, 1);
-    NVIC_EnableIRQ(EXTI9_5_IRQn);
-    
-    NVIC_SetPriority(TIM4_IRQn, 0);
+    NVIC_SetPriority(TIM4_IRQn, 1);
     NVIC_EnableIRQ(TIM4_IRQn);
 }
 
 void init_TIM3() {
-    TIM3->ARR = 60;
-    TIM3->CCR3 = 0;
+    //TIM3->PSC = 7999;
+    TIM3->ARR = 100;
+    TIM3->CCR3 = 70;
     TIM3->CCER |= TIM_CCER_CC3E; // включение выхода канала 3
     TIM3->CCMR2 |= TIM_CCMR2_OC3M_2 | TIM_CCMR2_OC3M_1; // режим 2 PWM
 }
@@ -110,36 +80,14 @@ void init_TIM4() {
     /* Для прерывыния необходимо сигнал TI1 снять триггером */
     TIM4->SMCR |= TIM_SMCR_TS_2;
     TIM4->DIER |= TIM_DIER_TIE;
-    
-    TIM4->ARR = 39;
-    TIM4->CNT = 0;
+    TIM4->ARR |= 100;
+    TIM4->CNT = 70;
+    TIM4->CR1 |= TIM_CR1_CEN;
 }
 
-void init_UART() {
-    // Включение USART3
-    USART3->CR1 |= USART_CR1_UE;
-    
-    // Нaстрока USART3
-    // 9600, 1 стоп бит, 9 бит данных, без проверки четности
-    USART3->BRR = 0x341; // Скорость работы 9600
-    USART3->CR1 &= ~USART_CR1_M; // 8 бит данных
-    USART3->CR1 &= ~USART_CR1_PCE; // без контроля четности
-    USART3->CR2 &= ~USART_CR2_STOP_1 & ~USART_CR2_STOP_0; // 1 стоп бит
-
-    // Разрешить передачу
-    USART3->CR1 |= USART_CR1_TE;
-}
-
-void send_byte(uint8_t byte) {
-    /* Ждем завершения предыдущей перелачи */
-    while(!(USART3->SR & USART_SR_TC));
-    /* Отправляем байт */
-    USART3->DR = byte;
-}
-
-void msDelay(int ms){
+void msDelay(uint32_t ms){
    while(ms-- > 0) {
-      volatile int x=500;
+      volatile short x = 500;
       while (x-- > 0)
          __asm("nop");
    }
